f78ff5770a65192ba297d3c63b59603fa5328872
Cristian Peña <cristiandpm.2004@gmail.com>
2024-04-10 12:22:20 -0500

    crea header, main y footer

---
+++ src/app/footer/footer.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    ContactanosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FooterModule { }
