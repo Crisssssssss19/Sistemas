25cf9509481b0993fd3cf53921cd80a4114edf2d
Cristian Peña <cristiandpm.2004@gmail.com>
2024-04-10 00:38:04 -0500

    Crea nav view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavegationViewModule } from './navegation-view/navegation-view.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavegationViewModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/navegation-view/navegation-view.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavegationViewComponent } from './navegation-view.component';
+
+
+
+@NgModule({
+  declarations: [NavegationViewComponent],
+  exports: [NavegationViewComponent]
+})
+export class NavegationViewModule { }
